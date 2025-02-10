ded7c6d53219df51a57e453277d2b6f8138b4697
ademayowa <ademayowaadeniyi@gmail.com>
2025-02-10 11:30:21 +0100

    chore: cleanups

--- components/Billing.tsx
+++ components/Billing.tsx
@@ -1,28 +1,29 @@
 import Image from 'next/image';
 import { apple, bill, google } from '../assets';
-import styles, { layout } from '../styles';
 
 const Billing = () => (
-  <section id='features' className={layout.sectionReverse}>
-    <div className={layout.sectionImgReverse}>
+  <section
+    id='features'
+    className='flex md:flex-row flex-col-reverse sm:py-16 py-6'
+  >
+    <div className='flex-1 flex justify-center items-center md:mr-10 mr-0 md:mt-0 mt-10 relative'>
       <img
         src='bill.png'
         alt='billing'
         className='w-[100%] h-[100%] relative z-[5]'
       />
 
-      {/* gradient start */}
+      {/* Billing gradient */}
       <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
       <div className='absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient' />
-      {/* gradient end */}
     </div>
 
-    <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>
+    <div className='flex-1 flex justify-center items-start flex-col'>
+      <h2 className='font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full'>
         Easily control your <br className='sm:block hidden' /> billing &
         invoicing
       </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      <p className='font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5'>
         Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
         neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
       </p>
